f189a738517e4f4bac9e4be8531be32c38b7628b
Aubrey Masten and Nicole Santamaria <aubrey@>
2017-04-24 09:20:52 -0700

    define initialize method will accept three parameters.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,7 @@
 class Triangle
-
+  define_method(:initialize) do |side1, side2, side3|
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,7 +2,9 @@ require('rspec')
 require('triangle')
 
 describe('Triangle#initialize') do
-
+  it('creates a new triangle object based on the three provided side arguments') do
+    expect(Triangle.new(3,4,5).class).to(eq(Triangle))
+  end
 end
 
 describe('Triangle#type') do
